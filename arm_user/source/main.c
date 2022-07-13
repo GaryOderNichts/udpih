d5769cac1a4ba521f2c7f4b1cbe09846c2f46d9c
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2022-07-13 23:48:09 +0200

    Adjustments for development mode consoles

--- arm_user/source/main.c
+++ arm_user/source/main.c
@@ -23,7 +23,7 @@ UhsServer* _main()
     blockHeader->size = 0x18d360;
     blockHeader->next = NULL;
 
-    UhsDevice* device = (UhsDevice*) 0x102996a0;
+    UhsDevice* device = (UhsDevice*) 0x10299700;
 
     // set some unfreeable blocks to NULL, so device destruction won't fail
     device->UhsDevStrings = NULL;
